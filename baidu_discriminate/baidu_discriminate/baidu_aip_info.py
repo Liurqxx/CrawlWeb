f5fe55bd255d477fdec02cc4dbecb3812587f324
2990599771@qq.com <2990599771@qq.com>
2018-03-22 15:47:57 +0800

    修改配置信息

--- baidu_discriminate/baidu_discriminate/baidu_aip_info.py
+++ baidu_discriminate/baidu_discriminate/baidu_aip_info.py
@@ -10,9 +10,10 @@ from aip import AipOcr
 class get_aipocr(object):
     def __init__(self):
         """ 你的 APPID AK SK """
-        self.APP_ID = '10823272'
-        self.API_KEY = 'aFCDfvS6SfknFkQUuXCr8tQ9'
-        self.SECRET_KEY = 'K6vjemA4Qw3wp6X23KvF1qCbdWSkK7kn'
+        #自己注册
+        self.APP_ID = '******'
+        self.API_KEY = '******'
+        self.SECRET_KEY = '******'
 
     def get_aipocr_info(self):
         return AipOcr(self.APP_ID, self.API_KEY, self.SECRET_KEY)
