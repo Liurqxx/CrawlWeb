76e4472b0e208bb2b66d0391184168d1112eeb75
2990599771@qq.com <2990599771@qq.com>
2018-04-03 12:35:28 +0800

    添加爬取电影天堂资源链接的爬虫工程

---
+++ dytt/dytt.py
@@ -0,0 +1,23 @@
+# _*_ coding:utf-8 _*_
+# Author:liu
+import requests
+import urllib
+import re
+
+'''
+    爬取电影天堂的电影
+'''
+
+# http://www.ygdy8.net/html/gndy/oumei/list_7_2.html
+# 下载页面
+# http://www.ygdy8.net/html/gndy/dyzz/20180314/56482.html
+
+
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+    main()
