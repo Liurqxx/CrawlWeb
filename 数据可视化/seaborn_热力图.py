b1cce5479365de844de4594149c6726a3ced77e6
2990599771@qq.com <2990599771@qq.com>
2018-04-05 20:13:16 +0800

    添加项目

---
+++ 数据可视化/seaborn_热力图.py
@@ -0,0 +1,19 @@
+# _*_ coding:utf-8 _*_
+# Author:liu
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+def main():
+    # 设置数据
+    df_iris = pd.read_csv(r'info.csv')
+
+    corrmat = df_iris[df_iris.columns[:4]].corr()
+
+    sns.heatmap(corrmat, square=True, linewidths=5, annot=True)
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
