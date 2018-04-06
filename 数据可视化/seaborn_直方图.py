eff8aeb7132905fc0bd7efc260998045cabcfd72
2990599771@qq.com <2990599771@qq.com>
2018-04-06 13:52:46 +0800

    修改数据源

--- 数据可视化/seaborn_直方图.py
+++ 数据可视化/seaborn_直方图.py
@@ -7,9 +7,9 @@ import pandas as pd
 
 def main():
     # 设置数据
-    df_iris = pd.read_csv(r'info.csv')
+    df_iris = pd.read_csv(r'iris.csv')
     # kde:密度线 rug：边际毛毯
-    sns.distplot(df_iris['Petal length'], kde=True, rug=True)
+    sns.distplot(df_iris['petal_length'], kde=True, rug=True)
     plt.show()
 
 
