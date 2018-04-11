31a828aaef3410f6b3d77771a51ff3276ff91d6e
2990599771@qq.com <2990599771@qq.com>
2018-04-11 14:18:56 +0800

    修改简单代码

--- 爬取电影天堂链接/dytt自动下载.py
+++ 爬取电影天堂链接/dytt自动下载.py
@@ -11,22 +11,11 @@ import threading
     爬取电影天堂的电影连接并下载
 '''
 
+
+# 第一页
+# http://www.ygdy8.net/html/gndy/oumei/list_7_1.html
+# 第二页
 # http://www.ygdy8.net/html/gndy/oumei/list_7_2.html
-# 下载页面
-# http://www.ygdy8.net/html/gndy/dyzz/20180314/56482.html
-
-
-'''
-    Headers:
-            Accept:image/webp,image/apng,image/*,*/*;q=0.8
-            Accept-Encoding:gzip, deflate
-            Accept-Language:zh-CN,zh;q=0.9
-            Connection:keep-alive
-            Cookie:u=Y4VsWhRogA0BAAAA8MJK
-            Host:log.he2d.com
-            Referer:http://www.37cs.com/html/click/8040_2134.html
-            User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36
-'''
 
 
 def down_move(url):
@@ -38,7 +27,7 @@ def down_move(url):
 def main():
     num = int(input("请输入页数:"))
     # 循环产生网址
-    for page in range(1, num+1):
+    for page in range(1, num + 1):
         # 网址
         url = 'http://www.ygdy8.net/html/gndy/oumei/list_7_' + str(page) + '.html'
         # 得到网页源代码
@@ -48,12 +37,13 @@ def main():
         # 提取网页源码中的下一页的网址信息
         # http://www.ygdy8.net
         # /html/gndy/dyzz/20180314/56482.html
-        # 得到 电影网址
+        # 得到电影网址列表
         dy_data = re.findall('<a href="(.*?)" class="ulink">', html.text)  # 返回的列表
+        print(dy_data)
         # 循环拼接电影网址
-        for m in dy_data:
+        for move_url in dy_data:
             # 拼接网址
-            xq_url = 'http://www.ygdy8.net' + m
+            xq_url = 'http://www.ygdy8.net' + move_url
             # 得到详情页的网页源代码
             html2 = requests.get(xq_url)
             html2.encoding = 'gb2312'
@@ -66,7 +56,8 @@ def main():
                 dy_link = dy_link.encode()
                 QutoUrl = urllib.request.quote(dy_link)
                 downurl = "thunder://" + (base64.b64encode(QutoUrl.encode())).decode()
-                print(downurl)
+                # 用于测试
+                # print(downurl)
                 # 开启线程调用迅雷下载资源
                 down_thread = threading.Thread(target=down_move, args=(downurl,))
                 # 开启线程
@@ -74,7 +65,6 @@ def main():
 
             except Exception as e:
                 print('数据有误', e)
-                # ftp: // ygdy8: ygdy8 @ yg45.dydytt.net:3065 / 阳光电影www.ygdy8.com.华盛顿邮报.BD.720p.中英双字幕.mkv
     print('添加下载任务完成')
 
 
