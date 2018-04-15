8e48812b49a1dd24fb72784ae52b34260b205737
2990599771@qq.com <2990599771@qq.com>
2018-04-15 19:25:30 +0800

    修改文件

--- 爬取电影天堂链接/dytt自动下载.py
+++ 爬取电影天堂链接/dytt自动下载.py
@@ -11,11 +11,11 @@ import threading
     爬取电影天堂的电影连接并下载
 '''
 
-
-# 第一页
-# http://www.ygdy8.net/html/gndy/oumei/list_7_1.html
-# 第二页
 # http://www.ygdy8.net/html/gndy/oumei/list_7_2.html
+# 下载页面
+# http://www.ygdy8.net/html/gndy/dyzz/20180314/56482.html
+
+
 
 
 def down_move(url):
@@ -27,7 +27,7 @@ def down_move(url):
 def main():
     num = int(input("请输入页数:"))
     # 循环产生网址
-    for page in range(1, num + 1):
+    for page in range(1, num+1):
         # 网址
         url = 'http://www.ygdy8.net/html/gndy/oumei/list_7_' + str(page) + '.html'
         # 得到网页源代码
@@ -37,13 +37,12 @@ def main():
         # 提取网页源码中的下一页的网址信息
         # http://www.ygdy8.net
         # /html/gndy/dyzz/20180314/56482.html
-        # 得到电影网址列表
+        # 得到 电影网址
         dy_data = re.findall('<a href="(.*?)" class="ulink">', html.text)  # 返回的列表
-        print(dy_data)
         # 循环拼接电影网址
-        for move_url in dy_data:
+        for m in dy_data:
             # 拼接网址
-            xq_url = 'http://www.ygdy8.net' + move_url
+            xq_url = 'http://www.ygdy8.net' + m
             # 得到详情页的网页源代码
             html2 = requests.get(xq_url)
             html2.encoding = 'gb2312'
@@ -56,8 +55,7 @@ def main():
                 dy_link = dy_link.encode()
                 QutoUrl = urllib.request.quote(dy_link)
                 downurl = "thunder://" + (base64.b64encode(QutoUrl.encode())).decode()
-                # 用于测试
-                # print(downurl)
+                print(downurl)
                 # 开启线程调用迅雷下载资源
                 down_thread = threading.Thread(target=down_move, args=(downurl,))
                 # 开启线程
@@ -65,6 +63,7 @@ def main():
 
             except Exception as e:
                 print('数据有误', e)
+               
     print('添加下载任务完成')
 
 
