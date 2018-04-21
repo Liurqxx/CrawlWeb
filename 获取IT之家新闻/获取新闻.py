8a2e65fa52c65422ac6995bfe26408fb4879b83b
2990599771@qq.com <2990599771@qq.com>
2018-04-21 22:33:23 +0800

    添加功能

--- 获取IT之家新闻/获取新闻.py
+++ 获取IT之家新闻/获取新闻.py
@@ -3,6 +3,29 @@
 import re
 import requests
 
+# 请求头信息
+headers = {
+
+    'user - agent': 'Mozilla / 5.0(Windows NT 10.0;WOW64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 64.0.3282.119Safari / 537.36'
+}
+
+
+# 获取数码之家主页所有信息
+def get_digi_info():
+    url = "https://digi.ithome.com/"
+    html = requests.get(url, headers=headers)
+    html.encoding = 'utf-8'
+    html = html.text
+
+    # 匹配得到时间、url、内容
+    news_url = re.findall(
+        r'<span class="date">(.*?)</span><span class="title"><a target="_blank" href="(.*?)">(<font color=.*?>)?(.*?)</a></span></li>',
+        html, re.S)
+
+    # 输出结果
+    for i in news_url:
+        print(i[0], i[1], i[3])
+
 
 def main():
 	# 数码之家信息
