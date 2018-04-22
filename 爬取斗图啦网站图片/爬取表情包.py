c75e414d42d535fa417345b01c006ab002b7c7e5
2990599771@qq.com <2990599771@qq.com>
2018-04-22 19:47:14 +0800

    添加功能

--- 爬取斗图啦网站图片/爬取表情包.py
+++ 爬取斗图啦网站图片/爬取表情包.py
@@ -2,6 +2,8 @@
 # Author:liu
 import re
 import requests
+import threading
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
