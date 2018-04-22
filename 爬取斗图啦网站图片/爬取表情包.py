0c3d9066f3cb99a0703e831d7ddccdffbcb18984
2990599771@qq.com <2990599771@qq.com>
2018-04-22 19:04:55 +0800

    添加工程

---
+++ 爬取斗图啦网站图片/爬取表情包.py
@@ -0,0 +1,16 @@
+# _*_coding:utf-8_*_
+# Author:liu
+import re
+import requests
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+}
+
+
+def main():
+  pass
+
+
+if __name__ == '__main__':
+    main()
