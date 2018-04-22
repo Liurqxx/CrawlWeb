e1d090e17f557dd2169e51d1816286dd3604c7ea
2990599771@qq.com <2990599771@qq.com>
2018-04-22 19:45:03 +0800

    添加功能

--- 爬取斗图啦网站图片/爬取表情包.py
+++ 爬取斗图啦网站图片/爬取表情包.py
@@ -28,7 +28,7 @@ def down_img(img_list):
 
 
 def main():
-      url_list = []
+    # 　循环页面
     for i in range(1, 10):
         # 　页面url
         page_url = "http://www.doutula.com/article/list/?page=" + str(i)
@@ -36,28 +36,13 @@ def main():
         html = requests.get(page_url, headers=headers)
         html.encoding = "utf-8"
         html = html.text
-        # print(html)
         # 匹配得到每页的表情数据
         url_title_list = re.findall(r'<a href="(.*?)" class="list-group-item random_list">', html)
-        # 把地址保存到列表内
-        url_list.append(url_title_list)
-    # 请求每个图片的连接
-    for i in url_list:
-        for img_data in i:
-            img_html_data = requests.get(img_data, headers=headers)
-            img_html_data.encoding = "utf-8"
-            img_html_data = img_html_data.text
-            # 　提取图片地址
-            img_url = re.findall(r'<meta property="og:image" content="(.*?)"/>', img_html_data)[0]
 
-            # 截取名字
-            # http://img.doutula.com/production/uploads/image//2018/04/20/20180420238483_aIMRwz.gif
-            img_name = img_url.split('/')[10]
-            # 下载图片
-            with open('./img/' + img_name, 'wb') as file:
-                img_info = requests.get(img_url, headers=headers).content
-                file.write(img_info)
-            print('下载成功...')
+        # 开启线程下载每页图片
+        t1 = threading.Thread(target=down_img, args=(url_title_list,))
+        t1.start()
+    t1.join()
 
 
 if __name__ == '__main__':
