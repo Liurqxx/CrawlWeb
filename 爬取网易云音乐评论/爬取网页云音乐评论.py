44cf91fa322bbb9602231b34d9b937398dc376c0
2990599771@qq.com <2990599771@qq.com>
2018-04-30 15:08:46 +0800

    修改文件

--- 爬取网易云音乐评论/爬取网页云音乐评论.py
+++ 爬取网易云音乐评论/爬取网页云音乐评论.py
@@ -4,6 +4,7 @@ import requests
 import json
 import matplotlib.pyplot as plt
 from pyecharts import Bar
+# from pyecharts import WordCloud
 from wordcloud import WordCloud
 
 '''
@@ -23,8 +24,7 @@ User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like 
 # 制作点赞数据图表
 def makePlot(nickname_list, likedCount_list):
     # 制成图表(柱状图)
-    bar = Bar('点赞图表')
-    # 添加数据
+    bar = Bar('点赞图表', width=1000, height=700)
     bar.add('点赞数', nickname_list, likedCount_list, is_stack=True, mark_line=['min', 'max'], mark_point=['average'])
     bar.render(path='./点赞图表分析.html')
 
@@ -32,14 +32,13 @@ def makePlot(nickname_list, likedCount_list):
 # 制成词云图显示
 def makeCloud(content_list):
     # 制成词云图显示
+
     content_list = "".join(content_list)
-    wordcloud = WordCloud(font_path=r'/usr/share/fonts/Fonts/simhei.ttf', max_words=300).generate(content_list)
-    # 设置相关属性
-    plt.figure()
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis('off')
-    # 保存图片
-    plt.savefig('./wordclound.png')
+    wordcloud = WordCloud(font_path=r'/usr/share/fonts/truetype/arphic/uming.ttc', width=1500, height=1000,
+                          max_font_size=300, min_font_size=20).generate(
+        content_list)
+    # 保存文件
+    wordcloud.to_file('./评论.png')
 
 
 def main():
@@ -61,7 +60,6 @@ def main():
         'encSecKey': 'cde658dde9306dee77aa07ae3797bfaa2e12ac3dead3a8a60d69c3df509cda2d136cf5ddc9105175f9d1b5f9de17ca715dd1b9bbccb3628003dfa6acb28c0aaf98d0d9b9f134692111975000365446283a4af6326d83f836c93a244222bb96dddafe82ce6053967b422d7abe77429baddefc434c279b475e7ff967a984564671'
 
     }
-
 
     # 获取网页数 url:评论地址 headers:请求头信息 data：请求参数
     response = requests.post(url, headers=headers, data=user_data)
