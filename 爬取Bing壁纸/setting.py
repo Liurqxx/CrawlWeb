2ce8f3903dfc03c31012c799678f6b7e387df0aa
2990599771@qq.com <2990599771@qq.com>
2018-05-02 22:45:26 +0800

    添加功能文件

---
+++ 爬取Bing壁纸/setting.py
@@ -0,0 +1,18 @@
+# _*_ coding:utf-8 _*_
+# Author:liu
+import os
+import sys
+
+'''
+    # 设置壁纸 file:....jpg:图片路径    gsettings set org.gnome.desktop.background picture-uri "file:/home/leon/pic/111.jpg"
+    os.system(
+        'gsettings set org.gnome.desktop.background picture-uri file:' + img_save_path + cd + '.jpg')
+
+'''
+
+
+
+
+
+if __name__ == '__main__':
+    main()
