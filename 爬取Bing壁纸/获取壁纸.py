2508f36b0b268bfd59a42bb813c437d21a5e6066
2990599771@qq.com <2990599771@qq.com>
2018-05-03 12:47:26 +0800

    修改文件

--- 爬取Bing壁纸/获取壁纸.py
+++ 爬取Bing壁纸/获取壁纸.py
@@ -1,8 +1,10 @@
+# encoding=utf-8
 import requests
 import os
 import datetime
 import re
 import shutil
+import time
 
 
 def main():
@@ -45,6 +47,7 @@ def main():
         # 设置壁纸 file:....jpg:图片路径    gsettings set org.gnome.desktop.background picture-uri "file:/home/leon/pic/111.jpg"
         os.system(
             'gsettings set org.gnome.desktop.background picture-uri file:' + img_save_path + cd + '.jpg')
+    print('完成时间', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
 
 
 if __name__ == '__main__':
