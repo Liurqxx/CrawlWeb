f7a59926e470c0ba12284ba2903e553a3c56d146
2990599771@qq.com <2990599771@qq.com>
2018-05-04 23:09:02 +0800

    添加代码

--- 下载知乎视频资源/下载知乎视频资源.py
+++ 下载知乎视频资源/下载知乎视频资源.py
@@ -11,6 +11,16 @@ import requests
 安装ffmpeg：sudo apt install ffmpeg
 '''
 
+def download(url):
+    video_ids = get_video_ids_from_url(url)
+    m3u8_list = list(yield_video_m3u8_url_from_video_ids(video_ids))
+    filename = '{}.mp4'.format(uuid.uuid4())
+    for idx, m3u8_url in enumerate(m3u8_list):
+        print('download {}'.format(m3u8_url))
+        subprocess.call(['ffmpeg', '-i', m3u8_url, filename.format(str(idx))])
+
+
+
 
 if __name__ == '__main__':
 
