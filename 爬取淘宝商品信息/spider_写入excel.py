f8e49c6596dc0d04906cf1033e4f520de5c717b6
2990599771@qq.com <2990599771@qq.com>
2018-05-14 22:26:52 +0800

    修改代码

--- 爬取淘宝商品信息/spider_写入excel.py
+++ 爬取淘宝商品信息/spider_写入excel.py
@@ -42,31 +42,42 @@ def main():
     # 创建工作表
     workbooke, worksheet = write_data()
 
-    n = 0
-
-    # 循环页数
-    for page in range(num):
-
-        # 拼装地址
-        url = "https://s.taobao.com/search?q={}&imgfile=&js=1&stats_click=search_radio_all%3A1&initiative_id=staobaoz_20180409&ie=utf8&bcoffset={}&ntoffset={}&p4ppushleft=1%2C48&s={}".format(
-            show_info, str(6 - (page * 3)), str(6 - (page * 3)), str(page * 44))
-
-        # 发送http请求
-        response = requests.get(url)
-        # 得到网页源码
-        response.encoding = 'utf-8'
-        html = response.text
-        # print(html)
-        # 正则匹配出数据
-        content = re.findall(r'g_page_config = (.*?)g_srp_loadCss', html, re.S)[0].strip()[:-1]
-        # print(content)
-        # 格式化json
-        content = json.loads(content)
-        # 获取商品信息列表
-        data_list = content['mods']['itemlist']['data']['auctions']
-        # print(data_list, len(data_list))
 
 
+        # 提取数据
+        # 标题 标价 购买人数 是否包邮 是否天猫 地区 店名 url
+        for index, item in enumerate(data_list):
+            try:
+                # 提取数据保存到字典中
+                temp = {
+                    'title': item['title'],
+                    'view_price': item['view_price'],
+                    'view_sales': item['view_sales'],
+                    'view_fee': '否' if float(item['view_fee']) else '是',
+                    'isTmall': '是' if item['shopcard']['isTmall'] else '否',
+                    'area': item['item_loc'],
+                    'name': item['nick'],
+                    'detail_url': item['detail_url'],
+                }
+                title = temp['title']
+                title = re.sub(r'</?.*?>', "", title, re.S)
+                i = n + index + 1
+
+                # 写入数据
+                worksheet.write(i, 0, title)
+                worksheet.write(i, 1, temp['view_price'])
+                worksheet.write(i, 2, temp['view_sales'])
+                worksheet.write(i, 3, temp['view_fee'])
+                worksheet.write(i, 4, temp['isTmall'])
+                worksheet.write(i, 5, temp['area'])
+                worksheet.write(i, 6, temp['name'])
+                worksheet.write(i, 7, temp['detail_url'])
+
+            except Exception as e:
+                print(e)
+        n += len(data_list)
+        print('------第%s页下载完成:----------' % str(page + 1))
+    workbooke.close()
 
 
 if __name__ == '__main__':
