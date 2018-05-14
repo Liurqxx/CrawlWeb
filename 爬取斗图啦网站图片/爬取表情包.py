5f3ed70b8367ad98c4edda1c94bf4e5bf56c4fb5
2990599771@qq.com <2990599771@qq.com>
2018-05-14 22:13:49 +0800

    修改代码

--- 爬取斗图啦网站图片/爬取表情包.py
+++ 爬取斗图啦网站图片/爬取表情包.py
@@ -10,30 +10,7 @@ headers = {
 }
 
 
-# 下载图片
-def down_img(img_list):
-    for img_data in img_list:
-        img_html_data = requests.get(img_data, headers=headers)
-        img_html_data.encoding = "utf-8"
-        img_html_data = img_html_data.text
-        # 　提取图片地址
-        img_url = re.findall(r'<meta property="og:image" content="(.*?)"/>', img_html_data)[0]
 
-        # 截取名字
-        # http://img.doutula.com/production/uploads/image//2018/04/20/20180420238483_aIMRwz.gif
-        img_name = img_url.split('/')[10]
-        # 下载图片
-        with open('./img/' + img_name, 'wb') as file:
-            img_info = requests.get(img_url, headers=headers).content
-            file.write(img_info)
-        print('下载成功...')
-
-
-def main():
-    all_page = int(input('请输入页数:'))
-    # 　循环页面
-    for i in range(1, all_page):
-        # 　页面url
         page_url = "http://www.doutula.com/article/list/?page=" + str(i)
         # 得到网页源代码
         html = requests.get(page_url, headers=headers)
