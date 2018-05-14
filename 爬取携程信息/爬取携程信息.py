392ea11b42c58a3e388210c98634170617f233b9
2990599771@qq.com <2990599771@qq.com>
2018-05-14 22:12:27 +0800

    修改代码

--- 爬取携程信息/爬取携程信息.py
+++ 爬取携程信息/爬取携程信息.py
@@ -25,6 +25,23 @@ print("评论总数为:{}".format(commentCounts))
 目标:获取酒店卫生评分、环境评分、服务评分、设施评分、用户推荐比、用户评分、评价内容
 '''
 
+url3 = 'http://m.ctrip.com/html5/hotel/HotelDetail/dianping/435383.html'
+
+html3 = urllib.request.urlopen(url3).read().decode('utf-8')
+
+soup_grades = BeautifulSoup(html3, 'lxml')
+
+# 获取酒店各项评分数据
+result_grades = soup_grades.find_all(attrs={"class": "ve-txt"})
+result_grades = str(result_grades)
+soup32 = BeautifulSoup(result_grades, 'lxml')
+
+result33 = soup32.find_all('em')
+userRecommendRate = result33[0].string
+hRating = result33[1].string
+eRating = result33[2].string
+sRating = result33[3].string
+iRating = result33[4].string
 
 print("用户推荐为:{}".format(userRecommendRate))
 print("卫生评分为:{}分".format(hRating))
@@ -32,16 +49,3 @@ print("环境评分为:{}分".format(eRating))
 print("服务评分为:{}分".format(sRating))
 print("设施评分为:{}分".format(iRating))
 
-# 提取用户评论数据
-
-
-
-for i in range(0, 10):
-    result_pf = soup_grades.find_all(attrs={"class": "g-ve"})
-    userRating = result_pf[i].string
-    print("评分为:{}".format(userRating))
-
-    # userName = result35[i].get_text()
-    result_data = soup_grades.find_all(attrs={"class": "tree-ellips-line6"})
-    commentText = result_data[i].get_text()
-    print("评论内容为:{}".format(commentText))
