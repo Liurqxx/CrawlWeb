77c048bc0096762648d64fbd43130fffe92d3161
2990599771@qq.com <2990599771@qq.com>
2018-05-15 16:34:29 +0800

    修改代码

--- 获取QQ说说/获取QQ说说.py
+++ 获取QQ说说/获取QQ说说.py
@@ -8,20 +8,6 @@ import requests
 import re
 
 
-
-
-    if a == True:
-        # 如果页面存在登录的DIV，则模拟登录
-        driver.switch_to.frame('login_frame')
-        driver.find_element_by_id('switcher_plogin').click()
-        driver.find_element_by_id('u').clear()  # 选择用户名框
-        driver.find_element_by_id('u').send_keys('QQ号')
-        driver.find_element_by_id('p').clear()
-        driver.find_element_by_id('p').send_keys('QQ密码')
-        driver.find_element_by_id('login_button').click()
-        time.sleep(3)
-    driver.implicitly_wait(3)
-
     # 判断好友空间是否设置了权限，通过判断是否存在元素ID：QM_OwnerInfo_Icon
     try:
         driver.find_element_by_id('QM_OwnerInfo_Icon')
