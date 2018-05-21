7c596eb9edbed079bca5e8c823c23a3074d5b070
2990599771@qq.com <2990599771@qq.com>
2018-05-21 21:03:14 +0800

    修改文件

--- 爬取电影评论/爬取电影短评论写入excel表格.py
+++ 爬取电影评论/爬取电影短评论写入excel表格.py
@@ -12,6 +12,29 @@ import xlsxwriter
 
 '''
 
+# 用于记录写入数据的条数
+data_cursor = 1
+
+
+# 创建工作文件
+def write_data():
+    # 删除文件
+    if os.path.exists('./复仇者联盟3评论信息.xlsx'):
+        os.remove('./复仇者联盟3评论信息.xlsx')
+
+    # 创建工作文件
+    workbooke = xlsxwriter.Workbook('复仇者联盟3评论信息.xlsx')
+    # 创建工作表
+    worksheet = workbooke.add_worksheet()
+    # 写标题
+    worksheet.write(0, 0, '是否有用')
+    worksheet.write(0, 1, '是否看过')
+    worksheet.write(0, 2, '作者')
+    worksheet.write(0, 3, '时间')
+    worksheet.write(0, 4, '内容')
+
+    return workbooke, worksheet
+
 
 def main():
     global data_cursor
